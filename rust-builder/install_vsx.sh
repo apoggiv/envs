d764424890ede588a4b5dd6b0324e9227cb8e91a
Arthur Poggi-Verignon <a.poggiverignon@fractalenergy.io>
2025-08-07 11:32:35 +0200

    Add copilot extension

--- rust-builder/install_vsx.sh
+++ rust-builder/install_vsx.sh
@@ -1,4 +1,5 @@
 code --install-extension tamasfe.even-better-toml && \
 code --install-extension mhutchie.git-graph && \
 code --install-extension eamodio.gitlens && \
+code --install-extension github.copilot && \
 code --install-extension rust-lang.rust-analyzer
